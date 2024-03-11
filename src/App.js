da545638e828b1ac6e4e29d0825a09cbaeb4a574
Anthony-Mbaya <120335962+Anthony-mbaya@users.noreply.github.com>
2024-03-11 16:00:31 +0300

    modified

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { useEffect,useState } from 'react';
 import Axios from 'axios';
@@ -7,27 +8,29 @@ import "./App.css";
 import 'react-dropdown/style.css';
 
 export default function App() {
-    const [data, setData] = useState([]);
+    const [info, setInfo] = useState([]);
     const [input,setInput] = useState(0);
     const [from,setFrom] = useState('usd');
     const [to,setTo] = useState('inr');
     const [options,setOptions] = useState([]);
     const [output,setOutput] = useState(0); 
-    
-    useEffect(() => {
-        Axios.get(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${from}.json`)
+    //https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${from}.json
+    //https://latest.currency-api.pages.dev/v1/currencies/${from}.json
+    useEffect(() => { 
+        Axios.get(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${from}.json`) 
         .then((res) => {
-            setData(res.data[from]);
-        });
+            setInfo(res.data[from]);
+            console.log(res.data[from]);
+        })
         },[from]);
 
     useEffect(() => {
-            setOptions(Object.keys(data));
+            setOptions(Object.keys(info));
             convert();
-        },[data])
+        },[info])
     
         function convert(){
-            var rate = data[to];
+            var rate = info[to];
             setOutput(input * rate);
         }
         function flip(){
@@ -65,4 +68,93 @@ export default function App() {
     )
 }
 
- 
+
+/*
+// App.js
+import { useEffect, useState } from 'react';
+import Axios from 'axios';
+import Dropdown from 'react-dropdown';
+import { HiSwitchHorizontal } from 'react-icons/hi';
+import 'react-dropdown/style.css';
+import './App.css';
+
+function App() {
+
+	// Initializing all the state variables 
+	const [info, setInfo] = useState([]);
+	const [input, setInput] = useState(0);
+	const [from, setFrom] = useState("usd");
+	const [to, setTo] = useState("inr");
+	const [options, setOptions] = useState([]);
+	const [output, setOutput] = useState(0);
+
+	// Calling the api whenever the dependency changes
+	useEffect(() => {
+		Axios.get(
+`https://latest.currency-api.pages.dev/v1/currencies/${from}.json`)
+			.then((res) => {
+				setInfo(res.info[from]);
+			})
+	}, [from]);
+
+	// Calling the convert function whenever
+	// a user switches the currency
+	useEffect(() => {
+		setOptions(Object.keys(info));
+		convert();
+	}, [info])
+
+	// Function to convert the currency
+	function convert() {
+		var rate = info[to];
+		setOutput(input * rate);
+	}
+
+	// Function to switch between two currency
+	function flip() {
+		var temp = from;
+		setFrom(to);
+		setTo(temp);
+	}
+
+	return (
+		<div className="App">
+			<div className="heading">
+				<h1>Currency converter</h1>
+			</div>
+			<div className="container">
+				<div className="left">
+					<h3>Amount</h3>
+					<input type="text"
+						placeholder="Enter the amount"
+						onChange={(e) => setInput(e.target.value)} />
+				</div>
+				<div className="middle">
+					<h3>From</h3>
+					<Dropdown options={options}
+						onChange={(e) => { setFrom(e.value) }}
+						value={from} placeholder="From" />
+				</div>
+				<div className="switch">
+					<HiSwitchHorizontal size="30px"
+						onClick={() => { flip() }} />
+				</div>
+				<div className="right">
+					<h3>To</h3>
+					<Dropdown options={options}
+						onChange={(e) => { setTo(e.value) }}
+						value={to} placeholder="To" />
+				</div>
+			</div>
+			<div className="result">
+				<button onClick={() => { convert() }}>Convert</button>
+				<h2>Converted Amount:</h2>
+				<p>{input + " " + from + " = " + output.toFixed(2) + " " + to}</p>
+
+			</div>
+		</div>
+	);
+}
+
+export default App;
+*/
