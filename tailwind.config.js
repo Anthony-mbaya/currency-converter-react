93477bb1d307450422de7bad010a615d5ec6f9ce
Anthony <anthonymwakulila@gmail.com>
2024-07-01 22:39:49 +0300

    conf for bg image

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        "bitcoin": "url('src/assets/images.jpg')",
+        "bitcoin": "url('bitcoin.avif')",
       },
     },
   },
